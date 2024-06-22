c9bfaaf6f4d8abe97497eaf988b8d8abbb65e605
Arun Murthy <arunsrinimurthy@gmail.com>
2024-06-22 09:52:45 -0400

    Lab tinkering commit /2

--- src/com/laboratory/TestPersonFilter.java
+++ src/com/laboratory/TestPersonFilter.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class TestPerson {
+public class TestPersonFilter {
 
     public static void main(String args[]) {
         List<Person> peopleList = new ArrayList<>();
