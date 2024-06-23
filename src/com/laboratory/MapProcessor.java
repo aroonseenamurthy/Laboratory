0ba33a2f01aa17ce062a3be880c37d9e30ce1a59
Arun Murthy <arunsrinimurthy@gmail.com>
2024-06-23 19:05:10 -0400

    Lab tinkering commit /4

---
+++ src/com/laboratory/MapProcessor.java
@@ -0,0 +1,17 @@
+package com.laboratory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProcessor {
+    public static void main(String[] args){
+
+        Map <Integer,String>map = new HashMap();
+        map.put(1,"abc");
+
+
+        map.forEach((key,val) -> System.out.println("Key " + key + " Value " +val));
+
+
+    }
+}
