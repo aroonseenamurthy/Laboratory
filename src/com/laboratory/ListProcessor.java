c83e05116242fc9aa11224b221016ed4f0f02913
Arun Murthy <arunsrinimurthy@gmail.com>
2024-06-23 17:55:34 -0400

    Lab tinkering commit /3

---
+++ src/com/laboratory/ListProcessor.java
@@ -0,0 +1,11 @@
+package com.laboratory;
+
+import java.util.Arrays;
+
+public class ListProcessor {
+
+    public static void main(String[] args){
+        Arrays.asList(1,2,3,4,5).stream().forEach(System.out::println);
+        Arrays.asList(1,2,3,4,5).stream().forEachOrdered(System.out::println);
+    }
+}
