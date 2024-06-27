b982e204c4723615c21760549698e391f6438944
Arun Murthy <arunsrinimurthy@gmail.com>
2024-06-26 21:56:56 -0400

    Lab tinkering commit /8

---
+++ src/com/laboratory/kata/FinalizeTest.java
@@ -0,0 +1,13 @@
+package com.laboratory.kata;
+
+public class FinalizeTest {
+    public static void main(String args[]){
+        try{
+
+        } catch (Exception ex){
+
+        } finally {
+            //super.finalize();
+        }
+    }
+}
